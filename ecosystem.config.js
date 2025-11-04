d0b5981c182092fa82d75a5b198a4fad7c129e6e
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2025-11-04 09:19:50 +0300

    feat(workflow): add a deployment workflow

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	apps: [
+		{
+			name: 'lynx-backend',
+			script: 'dist/main.js',
+			watch: true,
+			ignore_watch: ['node_modules', 'logs', 'data', 'dist'],
+			instances: 1,
+			autorestart: true,
+			max_memory_restart: '1G',
+			env: {
+				NODE_ENV: 'development',
+			},
+			env_production: {
+				NODE_ENV: 'production',
+			},
+			time: true,
+			restart_delay: 3000,
+			max_restarts: 10,
+		},
+	],
+}
